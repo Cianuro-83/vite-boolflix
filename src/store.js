d941f9e4a8e0ba8e3bcab93dc2734e420e6c514c
Cianuro-83 <buzzeosimone@yahoo.com>
2023-03-07 15:05:40 +0100

    IMPOSTAZIONE PROGETTO

---
+++ src/store.js
@@ -0,0 +1,13 @@
+import { reactive } from "vue";
+
+const store = reactive({
+  // characters: [],
+  // count: 0,
+  // pages: 0,
+  // currentPage: 1,
+  // search: '',
+  // selectedStatus: '',
+  // currentPage: 1,
+});
+
+export default store;
